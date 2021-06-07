9fdae8795336dafd2e036294779f72b063dab963
jahfunk <xknottykidx@gmail.com>
2021-06-07 22:26:45 +0200

    slash api

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -5,4 +5,4 @@ export const CAPITALIZED_APP =
 
 const URL = process.env.REACT_APP_SERVER_URL;
 
-export const SERVER_URL = URL ? `${URL}` : `http://localhost:5000/api`;
+export const SERVER_URL = URL ? `${URL}/api` : `http://localhost:5000/api`;
