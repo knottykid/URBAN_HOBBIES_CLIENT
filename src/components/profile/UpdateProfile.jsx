7cc6188fb0e52399120269f7ee53525ed9a4a907
jahfunk <xknottykidx@gmail.com>
2021-06-07 19:17:18 +0200

    profile component

---
+++ src/components/profile/UpdateProfile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import * as PROFILE_SERVICE from "../../services/profile";
+import * as CONSTS from "../../utils/consts";
+
+function UpdateProfile(props) {
+  const { user, authenticate } = props;
+  const [form, setForm] = React.useState({
+    username: user.username,
+  });
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const accessToken = localStorage.getItem(CONSTS.ACCESS_TOKEN);
+    PROFILE_SERVICE.UPDATE_PROFILE(form, accessToken)
+      .then((response) => {
+        console.log("response:", response);
+        authenticate(response.data.user);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username</label>
+        <input
+          name="username"
+          placeholder="Username"
+          value={form.username}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button>Update das profile</button>
+    </form>
+  );
+}
+
+export default UpdateProfile;
---
+++ src/components/profile/UploadPic.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const UploadPic = ({ user, setUser }) => {
+  const [picture, setPicture] = useState(null);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (!picture) {
+      console.log("you forgot to pick a picture");
+      return;
+    }
+    const formBody = new window.FormData();
+    formBody.append("profilePic", picture);
+    axios
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/uploadPicture/${user._id}`,
+        formBody
+      )
+      .then((res) => {
+        console.log(res);
+
+        setUser({ ...user, profilePic: res.data.picFromServer });
+      })
+      .catch((err) => console.log(err.response));
+  }
+
+  function handleInput(e) {
+    const image = e.target.files[0];
+    setPicture(image);
+  }
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input type="file" onChange={handleInput} />
+        <button type="submit">Upload your own Picture</button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadPic;
